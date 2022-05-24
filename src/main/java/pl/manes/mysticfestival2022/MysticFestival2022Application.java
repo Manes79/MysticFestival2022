0f4d177790295eb1b8710795117d2d2374c75a82
Manes79 <mszornel@gmail.com>
2022-05-24 19:03:21 +0200

    initial commmit

---
+++ src/main/java/pl/manes/mysticfestival2022/MysticFestival2022Application.java
@@ -0,0 +1,13 @@
+package pl.manes.mysticfestival2022;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysticFestival2022Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MysticFestival2022Application.class, args);
+    }
+
+}
---
+++ src/test/java/pl/manes/mysticfestival2022/MysticFestival2022ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.manes.mysticfestival2022;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysticFestival2022ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
