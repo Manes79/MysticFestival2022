b87ab8068aa6c151e65d36ac71192f57902aa087
Manes79 <mszornel@gmail.com>
2022-05-28 18:54:14 +0200

    new front-end usability --> back to top on page

--- mystic_front-end/src/pages/MysticFestivalBands.jsx
+++ mystic_front-end/src/pages/MysticFestivalBands.jsx
@@ -48,91 +48,91 @@ export const MysticFestivalBands = () => {
         <div className="body">
 
             <center>
-                <a href="#A" className="header_alphabet_links">
+                <a href={"#A"} className="header_alphabet_links">
                     <th>A</th>
                     <th></th>
                 </a>
-                <a href="#B" className="header_alphabet_links">
+                <a href={"#B"} className="header_alphabet_links">
                     <th>B</th>
                     <th></th>
                 </a>
-                <a href="#C" className="header_alphabet_links">
+                <a href={"#C"} className="header_alphabet_links">
                     <th>C</th>
                     <th></th>
                 </a>
-                <a href="#D" className="header_alphabet_links">
+                <a href={"#D"} className="header_alphabet_links">
                     <th>D</th>
                     <th></th>
                 </a>
-                <a href="#E" className="header_alphabet_links">
+                <a href={"#E"} className="header_alphabet_links">
                     <th>E</th>
                     <th></th>
                 </a>
-                <a href="#F" className="header_alphabet_links">
+                <a href={"#F"} className="header_alphabet_links">
                     <th>F</th>
                     <th></th>
                 </a>
-                <a href="#G" className="header_alphabet_links">
+                <a href={"#G"} className="header_alphabet_links">
                     <th>G</th>
                     <th></th>
                 </a>
-                <a href="#H" className="header_alphabet_links">
+                <a href={"#H"} className="header_alphabet_links">
                     <th>H</th>
                     <th></th>
                 </a>
-                <a href="#I" className="header_alphabet_links">
+                <a href={"#I"} className="header_alphabet_links">
                     <th>I</th>
                     <th></th>
                 </a>
-                <a href="#J" className="header_alphabet_links">
+                <a href={"#J"} className="header_alphabet_links">
                     <th>J</th>
                     <th></th>
                 </a>
-                <a href="#K" className="header_alphabet_links">
+                <a href={"#K"} className="header_alphabet_links">
                     <th>K</th>
                     <th></th>
                 </a>
-                <a href="#L" className="header_alphabet_links">
+                <a href={"#L"} className="header_alphabet_links">
                     <th>L</th>
                     <th></th>
                 </a>
-                <a href="#M" className="header_alphabet_links">
+                <a href={"#M"} className="header_alphabet_links">
                     <th>M</th>
                     <th></th>
                 </a>
-                <a href="#N" className="header_alphabet_links">
+                <a href={"#N"} className="header_alphabet_links">
                     <th>N</th>
                     <th></th>
                 </a>
-                <a href="#O" className="header_alphabet_links">
+                <a href={"#O"} className="header_alphabet_links">
                     <th>O</th>
                     <th></th>
                 </a>
-                <a href="#P" className="header_alphabet_links">
+                <a href={"#P"} className="header_alphabet_links">
                     <th>P</th>
                     <th></th>
                 </a>
-                <a href="#R" className="header_alphabet_links">
+                <a href={"#R"} className="header_alphabet_links">
                     <th>R</th>
                     <th></th>
                 </a>
-                <a href="#S" className="header_alphabet_links">
+                <a href={"#S"} className="header_alphabet_links">
                     <th>S</th>
                     <th></th>
                 </a>
-                <a href="#T" className="header_alphabet_links">
+                <a href={"#T"} className="header_alphabet_links">
                     <th>T</th>
                     <th></th>
                 </a>
-                <a href="#U" className="header_alphabet_links">
+                <a href={"#U"} className="header_alphabet_links">
                     <th>U</th>
                     <th></th>
                 </a>
-                <a href="#W" className="header_alphabet_links">
+                <a href={"#W"} className="header_alphabet_links">
                     <th>W</th>
                     <th></th>
                 </a>
-                <a href="#V" className="header_alphabet_links">
+                <a href={"#V"} className="header_alphabet_links">
                     <th>V</th>
                 </a>
             </center>
@@ -140,118 +140,114 @@ export const MysticFestivalBands = () => {
             <div>
 
                 <div id="B" className="row">
-                    <div className="col-12 col-lg-2 section-letter">
-                    </div>
                     <div className="header_alphabet">
                         <th>B</th>
                     </div>
+                    <a href={"#B"}>Powrót</a>
                     <BleedFromWithin displayBleedFromWithin={displayBleedFromWithin}></BleedFromWithin>
                 </div>
 
                 <div id="C" className="row">
-                    <div className="col-12 col-lg-2 section-letter">
-                    </div>
                     <div className="header_alphabet">
                         <th>C</th>
                     </div>
+                    <a href={"#B"}>Powrót</a>
                     <Carcass displayCarcass={displayCarcass}></Carcass>
+                    <a href={"#B"}>Powrót</a>
                     <CelticFrost displayCelticFrost={displayCelticFrost}></CelticFrost>
+                    <a href={"#B"}>Powrót</a>
                     <CodeOrange displayCodeOrange={displayCodeOrange}></CodeOrange>
                 </div>
 
                 <div id="H" className="row">
-                    <div className="col-12 col-lg-2 section-letter">
-                    </div>
                     <div className="header_alphabet">
                         <th>H</th>
                     </div>
+                    <a href={"#B"}>Powrót</a>
                     <Hellhammer displayHellhammer={displayHellhammer}></Hellhammer>
                 </div>
 
                 <div id="I" className="row">
-                    <div className="col-12 col-lg-2 section-letter">
-                    </div>
                     <div className="header_alphabet">
                         <th>I</th>
                     </div>
+                    <a href={"#B"}>Powrót</a>
                     <Igorrr displayIgorrr={displayIgorrr}></Igorrr>
                 </div>
 
                 <div id="J" className="row">
-                    <div className="col-12 col-lg-2 section-letter">
-                    </div>
                     <div className="header_alphabet">
                         <th>J</th>
                     </div>
+                    <a href={"#B"}>Powrót</a>
                     <JudasPriest displayJudasPriest={displayJudasPriest}></JudasPriest>
                 </div>
 
                 <div id="K" className="row">
-                    <div className="col-12 col-lg-2 section-letter">
-                    </div>
                     <div className="header_alphabet">
                         <th>K</th>
                     </div>
+                    <a href={"#B"}>Powrót</a>
                     <Katatonia displayKatatonia={displayKatatonia}></Katatonia>
+                    <a href={"#B"}>Powrót</a>
                     <KillingJoke displayKillilngJoke={displayKillingJoke}></KillingJoke>
                 </div>
 
                 <div id="M" className="row">
-                    <div className="col-12 col-lg-2 section-letter">
-                    </div>
                     <div className="header_alphabet">
                         <th>M</th>
                     </div>
+                    <a href={"#B"}>Powrót</a>
                     <Mastodon displayMastodon={displayMastodon}></Mastodon>
+                    <a href={"#B"}>Powrót</a>
                     <MercyfulFate displayMercyfulFate={displayMercyfulFate}></MercyfulFate>
+                    <a href={"#B"}>Powrót</a>
                     <Mgla displayMgla={displayMgla}></Mgla>
                 </div>
 
                 <div id="O" className="row">
-                    <div className="col-12 col-lg-2 section-letter">
-                    </div>
                     <div className="header_alphabet">
                         <th>O</th>
                     </div>
+                    <a href={"#B"}>Powrót</a>
                     <Obituary displayObituary={displayObituary}></Obituary>
+                    <a href={"#B"}>Powrót</a>
                     <Opeth displayOpeth={displayOpeth}></Opeth>
                 </div>
 
                 <div id="S" className="row">
-                    <div className="col-12 col-lg-2 section-letter">
-                    </div>
                     <div className="header_alphabet">
                         <th>S</th>
                     </div>
+                    <a href={"#B"}>Powrót</a>
                     <Saxon displaySaxon={displaySaxon}></Saxon>
                 </div>
 
                 <div id="T" className="row">
-                    <div className="col-12 col-lg-2 section-letter">
-                    </div>
                     <div className="header_alphabet">
                         <th>T</th>
                     </div>
+                    <a href={"#B"}>Powrót</a>
                     <TheMateria displayTheMateria={displayTheMateria}></TheMateria>
+                    <a href={"#B"}>Powrót</a>
                     <TheRavenAge displayTheRavenAge={displayTheRavenAge}></TheRavenAge>
+                    <a href={"#B"}>Powrót</a>
                     <Triptykon displayTriptykon={displayTriptykon}></Triptykon>
                 </div>
 
                 <div id="U" className="row">
-                    <div className="col-12 col-lg-2 section-letter">
-                    </div>
                     <div className="header_alphabet">
                         <th>U</th>
                     </div>
+                    <a href={"#B"}>Powrót</a>
                     <Urne displayUrne={displayUrne}></Urne>
                 </div>
 
                 <div id="V" className="row">
-                    <div className="col-12 col-lg-2 section-letter">
-                    </div>
                     <div className="header_alphabet">
                         <th>V</th>
                     </div>
+                    <a href={"#B"}>Powrót</a>
                     <Vader displayVader={displayVader}></Vader>
                 </div>
 
