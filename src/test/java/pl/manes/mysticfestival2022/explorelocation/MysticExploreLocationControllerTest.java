4bdc20ff01b61e0b098d170cdd154f14b62bd57a
Manes79 <mszornel@gmail.com>
2022-06-12 01:22:14 +0200

    MockMvc ---> test

--- src/test/java/pl/manes/mysticfestival2022/explorelocation/MysticExploreLocationControllerTest.java
+++ src/test/java/pl/manes/mysticfestival2022/explorelocation/MysticExploreLocationControllerTest.java
@@ -1,4 +1,4 @@
-package pl.manes.mysticfestival2022.bandlist;
+package pl.manes.mysticfestival2022.explorelocation;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,15 +11,16 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-class MysticBandListControllerTest {
+class MysticExploreLocationControllerTest {
 
     @Autowired
     private MockMvc mvc;
 
     @Test
     void shouldCorrectlyReturnStatus200() throws Exception {
-        mvc.perform(MockMvcRequestBuilders.get("/api/v1/mystic_festival/bandlist"))
+        mvc.perform(MockMvcRequestBuilders.get("/api/v1/mystic_festival/explore"))
                 .andDo(MockMvcResultHandlers.print())
                 .andExpect(MockMvcResultMatchers.status().is(200));
     }
+
 }
